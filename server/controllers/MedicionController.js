046be2433bf8cd3ddb6789ea8ad31e0f0ee4fb85
Rafa <alra.alderete@gmail.com>
2019-06-28 00:56:57 -0300

    fixes

--- server/controllers/MedicionController.js
+++ server/controllers/MedicionController.js
@@ -2,6 +2,18 @@ const Medicion = require('../models/Medicion.js');
 
 exports.getMedicion = async (req, res) => {
     const mediciones = await Medicion.find({}, { _id: 0 , __v: 0})
+    mediciones.sort(function (a, b) {
+        var key1 = a.timestamp;
+        var key2 = b.timestamp;
+    
+        if (key1 < key2) {
+            return -1;
+        } else if (key1 == key2) {
+            return 0;
+        } else {
+            return 1;
+        }
+    });
     console.log("Mediciones consultadas:");
     console.log(mediciones);
     res.send(mediciones);
@@ -9,17 +21,13 @@ exports.getMedicion = async (req, res) => {
 
 exports.postMedicion = async (req, res) => {
     console.log(Date.now());
-    const mediciones = req.body;
-    nuevasMediciones = [];
-    mediciones.forEach(function(medicion) {
-        var nivel = medicion.nivel;
-        var temperatura = medicion.temperatura;
-        var timestamp = medicion.timestamp * 1000;
-        var newMedicion = new Medicion({nivel, temperatura, timestamp});
-        nuevasMediciones.push(newMedicion);
-    });
-    await Medicion.insertMany(nuevasMediciones);
+    const medicion = req.body;
+    let nivel = medicion.nivel;
+    let temperatura = medicion.temperatura;
+    let timestamp = medicion.timestamp * 1000;
+    let newMedicion = new Medicion({nivel, temperatura, timestamp});
+    await newMedicion.save();
     console.log("Mediciones recibidas:");
-    console.log(nuevasMediciones);
+    console.log(newMedicion);
     res.send();
 };
